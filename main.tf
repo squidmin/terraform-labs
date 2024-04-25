ea86918de142fab8d58e25cf88f1ceef65b777e2
squidmin <morse.james.r@gmail.com>
2024-04-25 17:46:08 -0500

    Separate GCP secret manager configs

--- main.tf
+++ main.tf
@@ -32,24 +32,6 @@ resource "google_storage_bucket_iam_binding" "professional_portfolio_service_acc
 /* [END] GCP storage bucket IAM binding */
 
 
-/* [START] GCP secret manager */
-// Define a new secret with automatic replication.
-#resource "google_secret_manager_secret" "my_secret" {
-#  secret_id = "my-secret"
-#
-#  replication {
-#    automatic = true
-#  }
-#}
-
-// Add a secret version.
-#resource "google_secret_manager_secret_version" "my_secret_version" {
-#  secret      = google_secret_manager_secret.my_secret.id
-#  secret_data = "my super secret data"
-#}
-/* [END GCP secret manager] */
-
-
 /* [START] GCP project IAM member */
 resource "google_project_iam_member" "run_admin" {
   project = var.project_id
